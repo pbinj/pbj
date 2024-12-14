9f66a71eea2665bc71c6766482a5ca13b4a9dc27
Justin Spears <justin@augmentcode.com>
2024-12-14 00:28:06 +0000

    wip

--- packages/pbj-visualization/src/main.ts
+++ packages/pbj-visualization/src/main.ts
@@ -1,12 +1,15 @@
 import './assets/main.css'
-
+import VNetworkGraph from "v-network-graph"
+import "v-network-graph/lib/style.css"
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+const app = createApp(App);
+app.use(VNetworkGraph);
+
 
 app.use(createPinia())
 app.use(router)
