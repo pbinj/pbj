f39c99e7ba69ae4297a7304d33290ff15b694da8
Justin Spears <speajus@gmail.com>
2024-12-17 18:45:51 -0800

    fix lint

--- packages/pbj/src/errors.ts
+++ packages/pbj/src/errors.ts
@@ -1,16 +1,17 @@
 import { has } from "./guards.js";
 import { asString } from "./pbjKey.js";
-import { PBinJKey } from "./types.js";
+import type { PBinJKey } from "./types.js";
+
 const symbol = Symbol("@pbj/ServiceDescriptorProxy");
 
 export class PBinJAsyncError extends Error {
   [symbol] = true;
   constructor(
     public key: PBinJKey<any>,
-    public promise: Promise<any>,
+    public promise: Promise<any>
   ) {
     super(
-      `[${asString(key)} pending]: you have a async return from a service, please use context.resolveAsync`,
+      `[${asString(key)} pending]: you have a async return from a service, please use context.resolveAsync`
     );
     Object.setPrototypeOf(this, Error);
   }
