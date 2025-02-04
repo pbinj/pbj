4bcf95966b6ffd9e75bea2bba6cf5a7c299dd05d
Justin Spears <speajus@gmail.com>
2025-02-03 21:45:19 -0800

    better

--- packages/pbj-guards/src/optional.ts
+++ packages/pbj-guards/src/optional.ts
@@ -1,13 +1,13 @@
-import type { Guard } from "./guards.js";
+import { guardType, type Guard } from "./guards.js";
 
 export function optional<T>(this: Guard<T>): Guard<T | undefined> {
   const guard = this;
-  const ret = function optionalGuard(value: unknown): value is T | undefined {
+  function optionalGuard(value: unknown): value is T | undefined {
     return value == null || guard(value);
-  };
+  }
   // @ts-ignore
-  ret[guardType] = guard[guardType];
+  optionalGuard[guardType] = guard[guardType];
   // @ts-ignore
-  ret.toString = guard.toSchema;
-  return ret;
+  optionalGuard.toString = guard.toSchema;
+  return optionalGuard;
 }
