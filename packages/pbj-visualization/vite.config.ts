79ec65351a7ba52535852190f0796b0047c7cad9
Justin Spears <justin@augmentcode.com>
2025-04-09 18:11:47 -0700

    packaging madnes

--- packages/pbj-visualization/vite.config.ts
+++ packages/pbj-visualization/vite.config.ts
@@ -4,12 +4,11 @@ import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import vueDevTools from "vite-plugin-vue-devtools";
 import vuetify from "vite-plugin-vuetify";
-import tsConfigPaths from "vite-tsconfig-paths";
 import { context } from "@pbinj/pbj";
 import { register, serverConfigPBinJKey } from "./server/pbj.js";
 
 export const CONFIG = {
-  plugins: [tsConfigPaths(), vue(), vueJsx(), vueDevTools(), vuetify()],
+  plugins: [vue(), vueJsx(), vueDevTools(), vuetify()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
